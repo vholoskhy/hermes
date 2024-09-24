75b5333877a433db50b40f74ea246d2fc4c5367e
koliscom <vholoskhy@gmail.com>
2024-09-24 20:52:16 +0700

    navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,18 +2,30 @@ import Logo from "../../assets/image-72-1024x576.png";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="shadow-md bg-white dark:bg-yellow-900 dark:text-white duration-200 relative z-40">
       {/* upper Navbar */}
-      <div>
-        <div>
-          <div>
-            <a href="#">
-              <img src={Logo} alt="logo" />
+      <div className="bg-purple-400 py-2">
+        <div className="container flex justify-between items-center">
+          <div className="p-2">
+            <a
+              href="#"
+              className="font-bold text-2xl sm:text-3xl flex gap-2 flex justify-center items-center"
+            >
+              <img
+                src={Logo}
+                alt="logo"
+                className="w-100 uppercase h-20 rounded-md"
+              />
+              VholkhyShops
             </a>
           </div>
           <div>
-            <div>
-              <input type="text" />
+            <div className="group">
+              <input
+                type="text"
+                placeholder="search"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-blue-900 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary "
+              />
             </div>
           </div>
         </div>
